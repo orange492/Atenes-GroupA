48ad79957896358d22e049141b3e118e711147b5
skl509 <77062634+skl509@users.noreply.github.com>
2022-10-16 17:20:56 +0900

    JB 세팅

--- Minigame_Tower/Assets/Resources/Scripts/Blackjack/BlackjackManager.cs
+++ Minigame_Tower/Assets/Resources/Scripts/Blackjack/BlackjackManager.cs
@@ -1,42 +1,18 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using UnityEngine;
 
-namespace BlackJack // 네임스페이스는 클래스들을 그룹별로 묶어서 더 체계적으로 관리하기 위한것
+public class BlackjackManager : MonoBehaviour
 {
-    class BlackjackManager
+    // Start is called before the first frame update
+    void Start()
     {
-        private int numberOfDecks;
-        private Deck deck = null; // 덱에 있는거 이용
-        private Player player = null; // 플레이어에 있는거 이용
+        
+    }
 
-        public BlackjackManager()
-        {
-            
-        }
-
-        public void FillDeck() // 덱채우기
-        {
-           
-        }
-
-     
-        public Deck GetDeckList() //덱에서 리스트 읽어오기
-        {
-            return deck;
-        }
-
-       
-        public void SetNumberOfDecks(int num) // 덱의 수 설정하기
-        {
-            numberOfDecks = num;
-        }
-
-     
-    
-        public Player GetPlayer() 
-        {
-            return player;
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
