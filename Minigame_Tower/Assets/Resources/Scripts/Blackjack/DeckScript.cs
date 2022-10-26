0e636451ad6ec68138f01355b5c6158983784b0d
orange492 <40350423+orange492@users.noreply.github.com>
2022-10-26 13:35:21 +0900

    Revert "블랙잭 수정"
    This reverts commit cdc9e50f8bc443aee0ffe96e9bc19a427346ecfc, reversing
    changes made to f00db828f32f60ca4f60361c794897f81b771083.

---
+++ Minigame_Tower/Assets/Resources/Scripts/Blackjack/DeckScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckScript : MonoBehaviour
+{
+    public Sprite[] cardSprites; // 카드 53개 이미지 가져오기 배열로...
+    int[] cardValues = new int[53]; // 카드 값 생성해주기
+    int currentIndex = 0;//초기화 시키기
+
+    void Start()
+    {
+        GetCardValues(); // 카드값 읽어오기
+    }
+
+    void GetCardValues()
+    {
+        int num = 0;
+        // 
+        for (int i = 0; i < cardSprites.Length; i++)
+        {
+            num = i;
+            
+            num %= 13; // 총 12의 값으로 나와야되니깐...
+            
+            if(num > 10 || num == 0)
+            {
+                num = 10;
+            }
+            cardValues[i] = num++;
+        }
+    }
+
+    public void Shuffle() // 셔플해주기
+    {
+        
+        for(int i = cardSprites.Length -1; i > 0; --i)
+        {
+            int j = Mathf.FloorToInt(Random.Range(0.0f, 1.0f) * cardSprites.Length - 1) + 1;
+            Sprite face = cardSprites[i];
+            cardSprites[i] = cardSprites[j];
+            cardSprites[j] = face;
+
+            int value = cardValues[i];
+            cardValues[i] = cardValues[j];
+            cardValues[j] = value;
+        }
+        currentIndex = 1;
+    }
+
+    public int DealCard(CardScript cardScript) // 딜러카드 세팅해주기
+    {
+        cardScript.SetSprite(cardSprites[currentIndex]);
+        cardScript.SetValue(cardValues[currentIndex]);
+        currentIndex++;
+        return cardScript.GetValueOfCard();
+    }
+
+    public Sprite GetCardBack()
+    {
+        return cardSprites[0];
+    }
+}
---
+++ Minigame_Tower/Assets/Resources/Scripts/Puzzle/Characters/Character_Base.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Character_Base : MonoBehaviour
+{
+    public Sprite[] sprites;
+    SpriteRenderer sprite;
+    int animalType;
+    public Animator anim;
+    UnityEngine.Vector3 targetDir;
+    public int AnimalType
+    {
+        get => animalType;
+        set
+        {
+            animalType = value;
+        }
+    }
+
+    enum Animal
+    {
+        Bear,
+        Cat,
+        Deer,
+        Dog,
+        Duck
+        //Mouse,
+        //Panda,
+       // Pig,
+        //Rabbit
+    }
+    
+  
+
+
+    private void Awake()
+    {
+        sprite = transform.GetComponent<SpriteRenderer>();
+        
+        AnimalType = (int)Random.Range(0.0f, sprites.Length - 0.1f);
+        sprite.sprite = sprites[AnimalType];
+        anim = GetComponent<Animator>();
+     
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+      
+    }
+
+
+
+ public void AnimationActive(string direction)
+    {
+
+            anim.SetTrigger(direction);
+    
+       
+        
+
+    }
+}
