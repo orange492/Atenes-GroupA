c67b7a22408b695ba4d1c4e49a4401810c21722d
skl509 <skl509@naver.com>
2022-10-26 14:25:10 +0900

    블랙잭 확인

--- Minigame_Tower/Assets/Scripts/Blackjack/Core/BlackjackManager.cs
+++ Minigame_Tower/Assets/Scripts/Blackjack/Core/BlackjackManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BlackjackManager : Singletons<GameManager>
+public class BlackjackManager : Singletons<BlackjackManager>
 {
 
 
