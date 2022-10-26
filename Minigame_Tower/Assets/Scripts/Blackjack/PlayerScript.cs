b8c7e89b3ed0f801acd77f6631bf9bc19be65ee3
orange492 <40350423+orange492@users.noreply.github.com>
2022-10-26 13:58:07 +0900

    스크립트

--- Minigame_Tower/Assets/Scripts/Blackjack/PlayerScript.cs
+++ Minigame_Tower/Assets/Scripts/Blackjack/PlayerScript.cs
@@ -7,20 +7,20 @@ public class PlayerScript : MonoBehaviour
     // 해당 스크립트는 딜러랑 플레이어 스크립트다
 
     // 카드스크립트랑 덱 스크립트 불러오기(읽기)
-     public CardScript cardScript;   
-     public DeckScript deckScript;
+    public CardScript cardScript;
+    public DeckScript deckScript;
 
-    // Total value of player/dealer's hand
+   
     public int handValue = 0;
 
-    // Betting money
-    private int money = 1000;
 
-    // Array of card objects on table
+    public int money = 500; // 얼마 베팅할지
+
+  
     public GameObject[] hand;
-    // Index of next card to be turned over
+  
     public int cardIndex = 0;
-    // Tracking aces for 1 to 11 conversions
+
     List<CardScript> aceList = new List<CardScript>();
 
     public void StartHand()
@@ -29,60 +29,54 @@ public class PlayerScript : MonoBehaviour
         GetCard();
     }
 
-    // Add a hand to the player/dealer's hand
-    public int GetCard()
+ 
+    public int GetCard() //카드읽어오기
     {
-        // Get a card, use deal card to assign sprite and value to card on table
-        //int cardValue = deckScript.DealCard(hand[cardIndex].GetComponent<CardScript>());
-        // Show card on game screen
+    
         hand[cardIndex].GetComponent<Renderer>().enabled = true;
-        // Add card value to running total of the hand
-        // handValue += cardValue;
-        // If value is 1, it is an ace
-        // if(cardValue == 1)
+     
         {
             aceList.Add(hand[cardIndex].GetComponent<CardScript>());
         }
-        // Cehck if we should use an 11 instead of a 1
+
         AceCheck();
         cardIndex++;
         return handValue;
     }
 
-    // Search for needed ace conversions, 1 to 11 or vice versa
+
     public void AceCheck()
     {
-        // for each ace in the lsit check
+        
         foreach (CardScript ace in aceList)
         {
             if (handValue + 10 < 22 && ace.GetValueOfCard() == 1)
             {
-                // if converting, adjust card object value and hand
+                
                 ace.SetValue(11);
                 handValue += 10;
             }
             else if (handValue > 21 && ace.GetValueOfCard() == 11)
             {
-                // if converting, adjust gameobject value and hand value
+               
                 ace.SetValue(1);
                 handValue -= 10;
             }
         }
     }
 
-    // Add or subtract from money, for bets
+
     public void AdjustMoney(int amount)
     {
         money += amount;
     }
 
-    // Output players current money amount
+
     public int GetMoney()
     {
         return money;
     }
 
-    // Hides all cards, resets the needed variables
     public void ResetHand()
     {
         for (int i = 0; i < hand.Length; i++)
