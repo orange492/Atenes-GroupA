7e690381a64318b44242e3b2fec962250510d42e
qbamqbam <thetimelord@naver.com>
2022-10-26 09:53:05 +0900

    3매치 백업 재업로드

--- Minigame_Tower/Assets/Scripts/Puzzle/Block.cs
+++ Minigame_Tower/Assets/Scripts/Puzzle/Block.cs
@@ -1,6 +1,9 @@
+using UnityEngine;
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+
 
 
 public class Block : MonoBehaviour
@@ -9,6 +12,9 @@ public class Block : MonoBehaviour
     int indexX;
     int indexY;
     bool isThreeMatch = false;
+    BlockController blockController;
+
+
     public int IndexX
     {
         get => indexX;
@@ -47,17 +53,36 @@ public class Block : MonoBehaviour
 
     private void Start()
     {
-        Instantiate(characterPrefab, transform.position, transform.rotation, transform);
 
+        Instantiate(characterPrefab, transform.position, transform.rotation, transform);
+        blockController = FindObjectOfType<BlockController>();
 
     }
     private void Update()
     {
-        if (transform.childCount == 0)
+
+        if (transform.childCount == 0 && indexY == 0)
         {
-            //Instantiate(characterPrefab, transform.position, transform.rotation, transform);
+
+            Instantiate(characterPrefab, transform.position, transform.rotation, transform);
+            blockController.StartCoroutine(blockController.CharacterDown());
+
+
+
         }
-        //if (isThreeMatch)
+
+        if (isThreeMatch)
+        {
+
+            StartCoroutine(DestroyCharacter());
+
+        }
+        else
+        {
+            StopAllCoroutines();
+        }
+
+
         //{
         //    transform.GetChild(0).gameObject.SetActive(false);
         //}
@@ -65,6 +90,17 @@ public class Block : MonoBehaviour
         //{
         //    transform.GetComponentInChildren<Character_Base>().AnimalType
         //}
+    }
+
+    IEnumerator DestroyCharacter()
+    {
+
+        yield return new WaitForSeconds(0.5f);
+        if (transform.childCount != 0)
+        {
+            Destroy(transform.GetChild(0).gameObject); 
+        }
+        isThreeMatch = false;
     }
 
 
