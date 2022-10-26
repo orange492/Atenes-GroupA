8572bde76e3a3785d59c1b24f201529096832191
orange492 <40350423+orange492@users.noreply.github.com>
2022-10-26 09:48:07 +0900

    폴더정리

--- Minigame_Tower/Assets/Scripts/Blackjack/DeckScript.cs
+++ Minigame_Tower/Assets/Scripts/Blackjack/DeckScript.cs
@@ -1,31 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
 using UnityEngine;
 
 public class DeckScript : MonoBehaviour
 {
-    public Sprite[] cardSprites;
-    int[] cardValues = new int[53];
-    int currentIndex = 0;
+    public Sprite[] cardSprites; // 카드 53개 이미지 가져오기 배열로...
+    int[] cardValues = new int[53]; // 카드 값 생성해주기
+    int currentIndex = 0;//초기화 시키기
 
     void Start()
     {
-        GetCardValues();
+        GetCardValues(); // 카드값 읽어오기
     }
 
     void GetCardValues()
     {
         int num = 0;
-        // Loop to assign values to the cards
+        // 
         for (int i = 0; i < cardSprites.Length; i++)
         {
             num = i;
-            // Count up to the amout of cards, 52
-            num %= 13;
-            // if there is a remainder after x/13, then remainder
-            // is used as the value, unless over 10, the use 10
-            if (num > 10 || num == 0)
+            
+            num %= 13; // 총 12의 값으로 나와야되니깐...
+            
+            if(num > 10 || num == 0)
             {
                 num = 10;
             }
@@ -33,10 +31,10 @@ public class DeckScript : MonoBehaviour
         }
     }
 
-    public void Shuffle()
+    public void Shuffle() // 셔플해주기
     {
-        // Standard array data swapping technique
-        for (int i = cardSprites.Length - 1; i > 0; --i)
+        
+        for(int i = cardSprites.Length -1; i > 0; --i)
         {
             int j = Mathf.FloorToInt(Random.Range(0.0f, 1.0f) * cardSprites.Length - 1) + 1;
             Sprite face = cardSprites[i];
@@ -50,7 +48,7 @@ public class DeckScript : MonoBehaviour
         currentIndex = 1;
     }
 
-    public int DealCard(CardScript cardScript)
+    public int DealCard(CardScript cardScript) // 딜러카드 세팅해주기
     {
         cardScript.SetSprite(cardSprites[currentIndex]);
         cardScript.SetValue(cardValues[currentIndex]);
