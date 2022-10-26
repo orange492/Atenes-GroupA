4d8ff9f05385b50a17de0b5d3c3f7a4c99468c57
orange492 <40350423+orange492@users.noreply.github.com>
2022-10-26 13:35:14 +0900

    Revert "블랙잭 오류수정"
    This reverts commit e7e6cde32207fcfb8fec95eaeae5fb3692466823.

--- Minigame_Tower/Assets/Resources/Scripts/Blackjack/Core/GameManager.cs
+++ Minigame_Tower/Assets/Resources/Scripts/Blackjack/Core/GameManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : Singletons<GameManager>
+public class GameManager : Singleton1<GameManager>
 {
    
 
